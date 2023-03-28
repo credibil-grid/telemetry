322a0af736c687a38d6f32c3e95f7bfda11f207d
andrewweston <andrew.weston@propellerhead.co.nz>
2023-03-29 11:25:13 +1300

    update with latest

--- tracer.go
+++ tracer.go
@@ -1,30 +1,51 @@
-package tracer
+package main
 
 import (
 	"context"
 	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
+	"os"
 
+	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/codes"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
 	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
+	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/sdk/resource"
-	"go.opentelemetry.io/otel/sdk/trace"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
+	"go.opentelemetry.io/otel/trace"
 )
 
-// initiatises the OpenTelemetry client exporter
-func NewTracer(
-	endpointSuffix, serviceName, serviceVersion, environment string,
-) (*sdktrace.TracerProvider, error) {
+var tracer trace.Tracer
 
-	// OLTP exporter
+type ShutdownFunc func()
+
+// set instrumentation library name
+func init() {
+	tracer = otel.Tracer("credibil/instrumentation")
+}
+
+// Tracer is http middleware that adds OpenTelemetry tracing to each request.
+func Tracer(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx, span := tracer.Start(r.Context(), r.URL.Path, trace.WithSpanKind(trace.SpanKindServer))
+		defer span.End()
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+// InitTracer initiatises the OpenTelemetry exporter(s)
+func InitTracer() (shutdown ShutdownFunc, err error) {
+
+	// create OLTP http exporter
 	client := otlptracehttp.NewClient(
-		otlptracehttp.WithEndpoint("ca-otel.internal." + endpointSuffix),
+		otlptracehttp.WithEndpoint("ca-otel.internal." + os.Getenv("CONTAINER_APP_ENV_DNS_SUFFIX")),
+		// otlptracehttp.WithInsecure(),
 	)
 	traceExp, err := otlptrace.New(context.Background(), client)
 	if err != nil {
@@ -42,44 +63,55 @@ func NewTracer(
 		resource.Default(),
 		resource.NewWithAttributes(
 			semconv.SchemaURL,
-			semconv.ServiceName(serviceName),
-			semconv.ServiceVersion(serviceVersion),
-			attribute.String("environment", environment),
+			semconv.ServiceName(os.Getenv("CONTAINER_APP_NAME")),
+			semconv.ServiceVersion(os.Getenv("CONTAINER_APP_REVISION")),
+			attribute.String("environment", os.Getenv("CONTAINER_APP_ENV_DNS_SUFFIX")),
 		),
 	)
 	if err != nil {
 		return nil, fmt.Errorf("creating resource: %w", err)
 	}
 
-	return sdktrace.NewTracerProvider(
+	// create trace provider
+	tp := sdktrace.NewTracerProvider(
 		sdktrace.WithBatcher(traceExp),
 		sdktrace.WithBatcher(stdoutExp),
 		sdktrace.WithResource(res),
-	), nil
+	)
+	otel.SetTracerProvider(tp)
+	otel.SetTextMapPropagator(
+		propagation.NewCompositeTextMapPropagator(
+			propagation.TraceContext{},
+			propagation.Baggage{},
+		),
+	)
+
+	// return shutdown function
+	return func() {
+		if err := tp.Shutdown(context.Background()); err != nil {
+			log.Printf("error shutting down trace provider: %+v", err)
+		}
+	}, nil
 }
 
+// Implement a custom log writer to simplify log entries from the stdout exporter
 type logWriter struct{}
 type logSpan struct {
-	Name string
+	Name   string
+	Status sdktrace.Status
 	// Attributes []attribute.KeyValue
-	Attributes json.RawMessage
-	Status     trace.Status
 }
 
-// log error message (span.Status) to stdout for local debugging
+// Write logs the span.Status description for error spans
 func (w logWriter) Write(p []byte) (n int, err error) {
-
 	go func() {
-		// unmarshal span Status
 		var span logSpan
 		if err := json.Unmarshal(p, &span); err != nil {
 			log.Printf("error unmarshaling span: %v", err)
 		}
-
 		if span.Status.Code == codes.Error {
 			log.Printf("error in %s: %s", span.Name, span.Status.Description)
 		}
 	}()
-
 	return len(p), nil
 }
